43d25ec2ec976030d860010e72bb022346e31a04
buqusa <s2422@e-mirim.hs.kr>
2026-03-20 10:04:41 +0900

    counter HTML, JS

---
+++ 02_counter/main.js
@@ -0,0 +1,16 @@
+let number=0;
+
+// h1에 있는 숫자 요소를 가져오자
+// const resultH1 = document.getElementsByTadgName("h1")[0];
+const resultH1 = document.getElementById("result");
+// button에 있는 + 요소를 가져오자
+// const plusButton = document.getElementsByTagName("button")[0];
+const plusButton = document.getElementsByClassName("plus")[0]; //대괄호 꼭 있음(시험)
+// plusButton.addEventListener("click", ()=>{
+//     number++;
+//     resultH1.innerHTML = number;
+// });
+plusButton.onclick = ()=>{
+    number++;
+    resultH1.innerHTML = number;
+}
